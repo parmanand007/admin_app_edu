3b72d8e095a2f1905b597f3a2da6f3c3e100523d
parmanand007 <parmanandprajapati006@gmail.com>
2026-03-22 17:00:52 +0530

    feat(create): login & verfiy otp pages

--- src/app/providers/RouterProvider.tsx
+++ src/app/providers/RouterProvider.tsx
@@ -1,12 +1,21 @@
-// app/providers/RouterProvider.tsx
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import LoginPage from "../../features/auth/pages/LoginPage";
+import VerifyOtpPage from "../../features/auth/pages/VerifyOtpPage";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Hello</h1>} />
+        {/* Default route */}
+        <Route path="/" element={<Navigate to="/login" />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-otp" element={<VerifyOtpPage />} />
+
+        {/* Fallback */}
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
   );
